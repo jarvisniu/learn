8da93d4c9c3058603a6fdad5caecf93e093ec9b3
Jarvis Niu <jarvisniu@gmail.com>
2020-01-22 00:22:02 +0800

    add dotnet core

---
+++ dotnet-core/02_data_types/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace _02_data_types
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // String
+      string aFriend = "Bill";
+      Console.WriteLine("Hello " + aFriend);
+      // String Interpolation
+      Console.WriteLine($"Hello { aFriend }");
+      // Write Text File
+      using (var writer = File.OpenWrite("./friend.txt"))
+      {
+        var bytes = System.Text.Encoding.UTF8.GetBytes(aFriend);
+        writer.Write(bytes);
+      }
+    }
+  }
+}
---
+++ dotnet-core/README.md
@@ -0,0 +1,26 @@
+# dotnet-core
+
+> Microsoft .NET Core Software Development Platform
+
+## Links
+
+Homepage: https://dot.net/
+Docs: https://docs.microsoft.com/en-us/dotnet/
+GitHub: https://github.com/dotnet/core
+
+## Run
+
+> NOTE: You need to download and install the DotNET Core SDK firstly.
+
+```sh
+cd 01_hello
+dotnet run
+```
+
+## New Project
+
+```sh
+mkdir 02_hello
+cd 02_hello
+dotnet new console
+```
